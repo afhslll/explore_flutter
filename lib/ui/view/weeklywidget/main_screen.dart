6de9cee42cf71dfad0bd807ebda027ca61c943c6
Afifah Salimi <afifahsalimi@gmail.com>
2021-09-01 23:55:21 +0800

    FWoW - Wrap

--- lib/ui/view/weeklywidget/main_screen.dart
+++ lib/ui/view/weeklywidget/main_screen.dart
@@ -25,6 +25,27 @@ class MainScreen extends StatelessWidget {
                   ),
                   Container(height: 50, width: 50, color: Colors.orange),
                 ],
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+                width: double.infinity,
+                child: Wrap(
+                    spacing: 8.0,
+                    runSpacing: 4.0,
+                    alignment: WrapAlignment.center,
+                    runAlignment: WrapAlignment.center,
+                    direction: Axis.vertical,
+                    children: List.generate(
+                      7,
+                      (index) => Chip(
+                        avatar: CircleAvatar(
+                            backgroundColor: Colors.blue.shade900,
+                            child: const Icon(Icons.person,
+                                color: Colors.white, size: 16)),
+                        label: Text('Afifah $index'),
+                      ),
+                    )),
               )
             ],
           ),
