42fd640556a21962fd84bf8611cda916911985df
Afifah Salimi <afifahsalimi@gmail.com>
2021-09-05 07:18:36 +0800

    FWoW - Opacity, Animated Opacity

--- lib/ui/view/weeklywidget/main_screen.dart
+++ lib/ui/view/weeklywidget/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:explore_flutter/core/constant/path_constant.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   Alignment _alignment = Alignment.centerLeft;
+  double _carOpacity = 1.0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,62 +23,105 @@ class _MainScreenState extends State<MainScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                height: 50,
-                child: Row(
-                  children: [
-                    GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _alignment = Alignment.centerLeft;
-                          });
-                        },
-                        child: Container(width: 50, color: Colors.orange)),
-                    Expanded(
-                      child: AnimatedContainer(
-                          color: _alignment == Alignment.centerLeft
-                              ? Colors.yellow
-                              : Colors.green,
-                          alignment: _alignment,
-                          duration: const Duration(seconds: 2),
-                          curve: Curves.fastOutSlowIn,
-                          child: Text('Expanded')),
-                    ),
-                    GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _alignment = Alignment.centerRight;
-                          });
-                        },
-                        child: Container(width: 50, color: Colors.orange)),
-                  ],
-                ),
-              ),
-              Container(
-                color: Colors.blue,
-                height: 200,
-                width: double.infinity,
-                child: Wrap(
-                    spacing: 8.0,
-                    runSpacing: 4.0,
-                    alignment: WrapAlignment.center,
-                    runAlignment: WrapAlignment.center,
-                    direction: Axis.vertical,
-                    children: List.generate(
-                      7,
-                      (index) => Chip(
-                        avatar: CircleAvatar(
-                            backgroundColor: Colors.blue.shade900,
-                            child: const Icon(Icons.person,
-                                color: Colors.white, size: 16)),
-                        label: Text('Afifah $index'),
-                      ),
-                    )),
-              )
+              _animatedContainerExample(),
+              _wrapExapmle(),
+              _opacityExample(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _animatedContainerExample() {
+    return Container(
+      height: 50,
+      child: Row(
+        children: [
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  _alignment = Alignment.centerLeft;
+                });
+              },
+              child: Container(width: 50, color: Colors.orange)),
+          Expanded(
+            child: AnimatedContainer(
+                color: _alignment == Alignment.centerLeft
+                    ? Colors.yellow
+                    : Colors.green,
+                alignment: _alignment,
+                duration: const Duration(seconds: 2),
+                curve: Curves.fastOutSlowIn,
+                child: Text('Expanded')),
+          ),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  _alignment = Alignment.centerRight;
+                });
+              },
+              child: Container(width: 50, color: Colors.orange)),
+        ],
+      ),
+    );
+  }
+
+  Widget _wrapExapmle() {
+    return Container(
+      color: Colors.blue,
+      height: 200,
+      width: double.infinity,
+      child: Wrap(
+        spacing: 8.0,
+        runSpacing: 4.0,
+        alignment: WrapAlignment.center,
+        runAlignment: WrapAlignment.center,
+        direction: Axis.vertical,
+        children: List.generate(
+          7,
+          (index) => Chip(
+            avatar: CircleAvatar(
+                backgroundColor: Colors.blue.shade900,
+                child: const Icon(Icons.person, color: Colors.white, size: 16)),
+            label: Text('Afifah $index'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _opacityExample() {
+    return Row(
+      children: [
+        Opacity(
+          opacity: 0.5,
+          child: Image.asset(
+            ConstantAssetsString.carIcon,
+            height: 80,
+          ),
+        ),
+        AnimatedOpacity(
+          opacity: _carOpacity,
+          duration: const Duration(seconds: 2),
+          child: Image.asset(
+            ConstantAssetsString.carIcon,
+            height: 80,
+          ),
+        ),
+        Slider(
+          value: _carOpacity,
+          min: 0,
+          max: 1,
+          divisions: 5,
+          label: _carOpacity.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _carOpacity = value;
+            });
+          },
+        ),
+      ],
     );
   }
 }
